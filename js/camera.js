db009f39cbcc1960ebe6fc3fa5e7b5e9f67096cb
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 <arodriguezv@uni.pe>
2023-05-13 13:16:44 -0500

    demo camera 10

--- js/camera.js
+++ js/camera.js
@@ -1,66 +1,51 @@
 button_detection = document.querySelector("#snap")
 button_detection.disabled = true;
 
-// function startWebcam() {
-//   if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-//     navigator.mediaDevices.getUserMedia({video:{
-//       // width: 200,
-//       height:50,
-//       facingMode: 'environment'
-//   }})
-//   .then((stream) => {
-//    window.localStream = stream;
-//    webcam_video.srcObject = stream;
-//   })
-//   .catch((error) => {console.log(error)});
-//   button_detection.disabled = false;
-//   }
-//   else {
-//     console.log('La API de MediaDevices no es compatible con este navegador.');
-//   }
-// }
+const stopButton = document.getElementById("stopButton");
+stopButton.style.display = "none";
+
+const initButton = document.getElementById("initButton");
+
+const canvas = document.getElementById('canvas');
+var context = canvas.getContext('2d');
+const video = document.createElement('video');
+canvas.height = 400;
+canvas.width = 460;
+
+function drawVideoFrame() {
+  context.drawImage(video, 0, 0, canvas.width, canvas.height);
+  requestAnimationFrame(drawVideoFrame);
+}
 
 function startWebcam() {
   if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-    navigator.mediaDevices.enumerateDevices()
-      .then(function(devices) {
-        // Buscar la cámara con video disponible
-        const videoDevices = devices.filter(device => device.kind === 'videoinput');
+    navigator.mediaDevices.getUserMedia({video:{
+      // width: 400,
+      // height: 300,
+      facingMode: 'environment'
+  }})
+  .then((stream) => {
+   window.globalStream = stream;
+   video.srcObject = stream;
+   video.autoplay = true;
 
-        if (videoDevices.length > 0) {
-          const constraints = {
-            video: {
-              deviceId: videoDevices[0].deviceId,
-              width: { exact: 200 },
-              height: { exact: 50 },
-              facingMode: 'environment'
-            }
-          };
-
-          navigator.mediaDevices.getUserMedia(constraints)
-            .then(function(stream) {
-              window.localStream = stream;
-              webcam_video.srcObject = stream;
-              button_detection.disabled = false;
-            })
-            .catch(function(error) {
-              console.log('Error al acceder a la cámara:', error);
-            });
-        } else {
-          console.log('No se encontró ninguna cámara disponible.');
-        }
-      })
-      .catch(function(error) {
-        console.log('Error al enumerar dispositivos:', error);
-      });
-  } else {
+  video.addEventListener('loadedmetadata', function() {
+    drawVideoFrame();
+  });
+  })
+  .catch((error) => {console.log(error)});
+  button_detection.disabled = false;
+  initButton.style.display = "none";
+  stopButton.style.display = "block";
+  }
+  else {
     console.log('La API de MediaDevices no es compatible con este navegador.');
   }
 }
 
 function stopWebcam() {
-  localStream.getVideoTracks()[0].stop();
-  webcam_video.src = '';
-  context.clearRect(0, 0,200,50);
+  stopButton.style.display = "none";
+  initButton.style.display = "block";
+  globalStream.getVideoTracks()[0].stop();
   button_detection.disabled = true;
 }
--- js/main.js
+++ js/main.js
@@ -1,22 +1,11 @@
   
-// init camera
-const webcam_video = document.querySelector('#webcam__video')
-// webcam_video.style.transform = "scaleX(-1)";
-
-// draw image 
 const snap = document.getElementById("snap");
-const canvas2 = document.getElementById('canvas2');
-var context = canvas2.getContext('2d');
-// canvas2.style.transform = "scaleX(-1)";
-
 const texto_insertado = document.getElementById("textoInsertado");
 
 // var contador = []
 snap.addEventListener("click", function() {
-    var countmatrix = [];
-    
-    context.drawImage(webcam_video, 0, 0); // generate image
-    const dataURL = canvas2.toDataURL(); // generate base 64
+     // generate image
+    const dataURL = canvas.toDataURL(); // generate base 64
     console.log(dataURL);
 
     const formData = new FormData();
@@ -25,23 +14,19 @@ snap.addEventListener("click", function() {
     formData.append("apikey"  , "K87428168588957");
     fetch('https://api.ocr.space/parse/image',{
         method: 'POST', 
-        // headers: {
-        //  'Content-type': 'application/json',
-        //  'Accept': 'application/json'
-        // },
+
         body: formData
-        // body: JSON.stringify({"image":dataURL})
       })
    .then((response) => response.json())
    .then((ocrParsedResult) => 
    {
     // Obtener los resultados analizados, código de salida de OCR y otros datos
     const parsedResults = ocrParsedResult["ParsedResults"];
-    const ocrExitCode = ocrParsedResult["OCRExitCode"];
-    const isErroredOnProcessing = ocrParsedResult["IsErroredOnProcessing"];
-    const errorMessage = ocrParsedResult["ErrorMessage"];
-    const errorDetails = ocrParsedResult["ErrorDetails"];
-    const processingTimeInMilliseconds = ocrParsedResult["ProcessingTimeInMilliseconds"];
+    // const ocrExitCode = ocrParsedResult["OCRExitCode"];
+    // const isErroredOnProcessing = ocrParsedResult["IsErroredOnProcessing"];
+    // const errorMessage = ocrParsedResult["ErrorMessage"];
+    // const errorDetails = ocrParsedResult["ErrorDetails"];
+    // const processingTimeInMilliseconds = ocrParsedResult["ProcessingTimeInMilliseconds"];
     
     console.log('Estamos en la respuesta de la API:');
     texto_insertado.innerHTML = parsedResults[0]['ParsedText'];
