3f62655498d4ab88a8106e23174f9305317a110f
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 <arodriguezv@uni.pe>
2023-05-13 20:53:07 -0500

    demo 14

--- js/camera.js
+++ js/camera.js
@@ -13,7 +13,30 @@ canvas.height = 320;
 canvas.width = 320;
 
 function drawVideoFrame() {
+  // // context.clearRect(0, 0, canvas.width, canvas.height);
+  // context.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+  // // Aplicar efecto de desenfoque a todo el canvas
+  // context.filter = 'blur(5px)';
+  // context.globalAlpha = 0.7;
+
+
+  // // Dibujar la ventana de 200x50 sin efecto de desenfoque
+  // const windowWidth = 200;
+  // const windowHeight = 50;
+  // const windowX = (canvas.width - windowWidth) / 2;
+  // const windowY = (canvas.height - windowHeight) / 2;
+  // context.clearRect(0, 0, canvas.width, canvas.height);
+  
+  // Aplicar efecto de desenfoque a la mitad izquierda del canvas
+  context.filter = 'blur(5px)';
   context.drawImage(video, 0, 0, canvas.width, canvas.height);
+  // context.fillRect(0, 0, canvas.width, canvas.height);
+
+  // Dibujar la cámara sin efecto de desenfoque en la mitad derecha del canvas
+  context.filter = 'none';
+  context.drawImage(video, 240, 80, 240, 80, 0, 220, 320, 60);
+
   requestAnimationFrame(drawVideoFrame);
 }
 
